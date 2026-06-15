2576b820d7e0500215bb2af1a4315a33c1384af6
RiskyRexx <user.rishikeshpalkar87gmail.com>
2026-06-15 22:31:19 +0530

    Configured Service and Controller Layer with responsive Web pages

---
+++ src/main/java/org/ycce_a1_sms/student_management_system/Service/StudentServiceImpl.java
@@ -0,0 +1,43 @@
+package org.ycce_a1_sms.student_management_system.Service;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.ycce_a1_sms.student_management_system.Repository.StudentRepository;
+import org.ycce_a1_sms.student_management_system.entity.Student;
+
+@Service
+public class StudentServiceImpl implements StudentService {
+
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentServiceImpl(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    @Override
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
+
+    @Override
+    public Student saveStudent(Student student) {
+        return studentRepository.save(student);
+    }
+
+    @Override
+    public Student getStudentById(Integer id) {
+        return studentRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Student updateStudent(Student student) {
+        return studentRepository.save(student);
+    }
+
+    @Override
+    public void deleteStudentById(Integer id) {
+        studentRepository.deleteById(id);
+    }
+}
